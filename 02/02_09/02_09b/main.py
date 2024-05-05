42f7e5ad6a464bf31c5c8eebf833271a99c11dea
Justin Toliver <106632392+BeterMain@users.noreply.github.com>
2024-05-05 22:14:58 +0000

    Completed Chapter 2 Challenge

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,17 @@
 def find_second_smallest(my_list):
-    return 0
+    if len(my_list) < 1:
+        return None
 
-print(find_second_smallest([5, 8, 3, 2, 6]))
+    smallest = my_list[0]
+    second_smallest = smallest
+
+    for num in my_list:
+        if num < smallest:
+            second_smallest = smallest
+            smallest = num
+        elif num < second_smallest:
+            second_smallest = num
+
+    return second_smallest
+
+print(find_second_smallest([1, 2, 0, -1]))
